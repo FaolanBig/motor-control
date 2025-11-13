b6ed92055a0d1ad1224626f45f109794055f66b7
faolanbig <faolan.big@web.de>
2025-11-13 17:41:10 +0100

    should be good

--- motor-control.ino
+++ motor-control.ino
@@ -1,33 +1,108 @@
 #define FORWARDS = true
 #define BACKWARDS = false
 
+// { switch A1, A2, B1, B2. interrupt pin }
 int HBA[] = {2, 3, 4, 5};
 int HBB[] = {6, 7, 8, 9};
 int HBC[] = {10, 11, 12, 13};
 
 
+// switching params
+volatile bool HBX_to_switch[] = { false, false, false };
+volatile int HBX_cycle_state = 0;
+volatile bool HBX_second_half = false;
+
+// interrupt array
+int HBX_interrupt_pins[] = { 18, 19, 20 };
+
 void setup()
 {
-    for (auto &&i : HBA)
-    {
-        pinMode(i, OUTPUT);
-    }
-    for (auto &&i : HBB)
-    {
-        pinMode(i, OUTPUT);
-    }
-    for (auto &&i : HBC)
-    {
-        pinMode(i, OUTPUT);
-    }
+    for (auto &&i : HBA) { pinMode(i, OUTPUT); }
+    for (auto &&i : HBB) { pinMode(i, OUTPUT); }
+    for (auto &&i : HBC) { pinMode(i, OUTPUT); }
+
+    // initializing interrupts
+    attachInterrupt(digitalPinToInterrupt(HBX_interrupt_pins[0]), switch_A, RISING); // A
+    attachInterrupt(digitalPinToInterrupt(HBX_interrupt_pins[0]), switch_B, RISING); // B
+    attachInterrupt(digitalPinToInterrupt(HBX_interrupt_pins[0]), switch_C, RISING); // C
 }
 
 void loop()
 {
-    
+    switch (HBX_cycle_state)
+    {
+    case 0:
+        switchPair(HBA, !HBX_second_half);
+        switchPair(HBB, HBX_second_half);
+        switchPair(HBC, false, true);
+        break;
+    case 1:
+        switchPair(HBA, false, true);
+        switchPair(HBB, !HBX_second_half);
+        switchPair(HBC, HBX_second_half);
+        break;
+    case 2:
+        switchPair(HBA, HBX_second_half);
+        switchPair(HBB, false, true);
+        switchPair(HBC, !HBX_second_half);
+        break;
+    default:
+        break;
+    }
 }
 
-void switchPair(int HBX[4], bool direction)
+void switchPair(int HBX[4], bool direction = false, bool off = false)
 {
-    digitalWrite()
+    auto level = HIGH;
+    auto level2 = HIGH;
+    if (off)
+    {
+        level = LOW;
+        level2 = LOW;
+    }
+    else
+    {
+        if (direction)
+        {
+            level = HIGH; 
+            level2 = LOW;
+        }
+        else 
+        { 
+            level = LOW;
+            level2 = HIGH;
+        }
+    }
+    // level = inverted ? HIGH : LOW;
+    
+    for (int i = 0; i < 2; i++)
+    {
+        pinMode(HBX[i * 2], level); // normal level
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        pinMode(HBX[(i * 2) + 1], level2); // usually inverted level
+    }
+}
+
+void switch_A()
+{
+    // if (HBX_second_half) { HBX_cycle_state = 0; }
+    // else { HBX_cycle_state = 3; }
+    HBX_cycle_state = 0;
+    HBX_second_half = !HBX_second_half;
+}
+void switch_B()
+{
+    // if (HBX_second_half) { HBX_cycle_state = 1; }
+    // else { HBX_cycle_state = 4; }
+    HBX_cycle_state = 1;
+    HBX_second_half = !HBX_second_half;
+}
+void switch_C()
+{
+    // if (HBX_second_half) { HBX_cycle_state = 2; }
+    // else { HBX_cycle_state = 5; }
+    HBX_cycle_state = 2;
+    HBX_second_half = !HBX_second_half;
 }
